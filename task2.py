90ee4054c2c22768e66e58fa026015363884c4e5
Anton Shvechikhin <antonkamski@gmail.com>
2022-11-09 01:07:22 +0400

    Added histogram construction

--- task2.py
+++ task2.py
@@ -6,7 +6,6 @@ amount_of_strings = 0
 amount_of_numbers = 0
 vectorsList = pandas.read_csv('vectors.csv', header=None).__array__(dtype=float)
 
-
 def calc_euclidean_distance_between_all_pairs():
     """This function calculates the Euclidean distance between all pairs of distinct vectors in vectors.csv."""
     current_vector_index = 0
@@ -18,10 +17,10 @@ def calc_euclidean_distance_between_all_pairs():
     first_max_euclidean_distance_index = 0
     second_max_euclidean_distance_index = 0
     vectors_dictionary = {}
-
+    amount_of_vector_pairs = 0
     while current_vector_index < len(vectorsList):
         while second_current_vector_index < len(vectorsList):
-            if all(vectorsList[current_vector_index]) == all(vectorsList[second_current_vector_index]):
+            if ((vectorsList[current_vector_index]) != (vectorsList[second_current_vector_index])).all():
                 print(
                     'Euclidean distance for vectors under indices ' + str(current_vector_index) +
                     ' and ' + str(second_current_vector_index) + ':')
@@ -53,6 +52,7 @@ def calc_euclidean_distance_between_all_pairs():
                                                                vectorsList[second_current_vector_index])
                     first_max_euclidean_distance_index = current_vector_index
                     second_max_euclidean_distance_index = second_current_vector_index
+                amount_of_vector_pairs += 1
             second_current_vector_index += 1
         current_vector_index += 1
         second_current_vector_index = current_vector_index + 1
@@ -63,6 +63,7 @@ def calc_euclidean_distance_between_all_pairs():
           + str(first_max_euclidean_distance_index) + ' and ' + str(second_max_euclidean_distance_index) + '. '
           'The value of the Euclidean distance for this pair of vectors is ' + str(max_euclidean_distance) + '.')
 
+    print(amount_of_vector_pairs)
     plt.title('Distribution of Euclidean distances')
     plt.xlabel('Euclidean distance')
     plt.ylabel('Amount of pairs')
